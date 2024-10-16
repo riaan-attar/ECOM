b2a0fa699c5e313838b10517d86faa350c0d1723
Riaan Attar <attarriaan6226@gmail.com>
2024-10-16 11:22:37 +0530

    Added cart,checkout,index page

--- ECOM/urls.py
+++ ECOM/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from store import views
+import store.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
